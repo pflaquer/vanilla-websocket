490262f7b54bda8eddfe5dd844357d1e47348969
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-02-01 11:38:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+/*
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
@@ -8,4 +10,27 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.send('something');
+});
+*/
+
+var server = require('websocket').server, http = require('http');
+
+var socket = new server({  
+    httpServer: http.createServer().listen(1337)
+});
+
+socket.on('request', function(request) {  
+    var connection = request.accept(null, request.origin);
+
+    connection.on('message', function(message) {
+        console.log(message.utf8Data);
+        connection.sendUTF('hello');
+        setTimeout(function() {
+            connection.sendUTF('this is a websocket example');
+        }, 1000);
+    });
+
+    connection.on('close', function(connection) {
+        console.log('connection closed');
+    });
 });
